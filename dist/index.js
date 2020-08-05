a82c47bc35e27f446614d516e21d591708ed952c
Luke Hansford <mail@lukehansford.me>
2020-08-05 12:28:13 +0200

    Fix some build config stuff

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+const Example = ({
+  name
+}) => /*#__PURE__*/React.createElement(React.Fragment, null, name);
+
+module.exports = Example;
