c001c32d10012b20a34d96d85071d10509e17596
Luke Hansford <mail@lukehansford.me>
2020-08-05 09:54:16 +0200

    Add storybook

---
+++ src/index.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Example from './index';
+
+export default {
+  title: 'Example',
+  component: Example,
+};
+
+export const ExampleStory = (): JSX.Element => <Example name="example" />;
